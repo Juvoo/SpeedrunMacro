4ca3fa55cb6a192b5f966f6d0427013588f620e0
Juvoo <lkinghouser@gmail.com>
2022-12-27 14:25:10 -0600

    Added Multi Instance

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import time
 
+import pynput.mouse
+
 from pynput import keyboard
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key
 
 import subprocess
 
+import multi_instance
 
-controller = Controller()
+
+keyboardController = pynput.keyboard.Controller()
+mouseController = pynput.mouse.Controller()
 
 taigaSeed = "2483313382402348964"
 dolphinSeed = "-4530634556500121041"
@@ -15,41 +20,40 @@ vineSeed = "8398967436125155523"
 
 seed = taigaSeed
 
+multi_instance_enabled = True
 
 def press_key(key):
-    controller.press(key)
-    controller.release(key)
+    keyboardController.press(key)
+    keyboardController.release(key)
+    time.sleep(0.01)
+
+
+def press_button(button):
+    mouseController.press(button)
+    mouseController.release(button)
+    time.sleep(0.01)
+
+
+def move_to_point(x, y):
+    mouseController.move(x, y)
     time.sleep(0.01)
 
 
 def type_seed():
-    controller.type(seed)
+    type_string(seed)
+
+
+def type_string(string):
+    keyboardController.type(string)
     time.sleep(0.01)
 
 
-def get_focused_application_name():
-    cmd = """
-    tell application "System Events"
-      set windowTitle to ""
-      set frontApp to first application process whose frontmost is true
-      set frontAppName to name of frontApp
-      tell process frontAppName
-        tell (1st window whose value of attribute "AXMain" is true)
-          set windowTitle to value of attribute "AXTitle"
-        end tell
-      end tell
-    log frontAppName
-    end tell
-    """
-
-    result = subprocess.run(['osascript', '-e', cmd])
-    return result.stdout
+def minecraft_window_focused():
+    p = subprocess.run(['osascript', '-e', 'tell application "System Events"', '-e', 'set windowTitle to name of (front window of (first application process whose frontmost is true))', '-e', 'copy windowTitle to stdout', '-e', 'end tell'], capture_output=True)
+    return "Minecraft" in p.stdout.__str__()
 
 
-print(get_focused_application_name())
-
-
-def execute():
+def execute_ssg():
     press_key(Key.tab)
     press_key(Key.enter)
     for i in range(3):
@@ -71,6 +75,62 @@ def execute():
         press_key(Key.tab)
     press_key(Key.enter)
 
+    if multi_instance_enabled:
+        multi_instance.switch_to_next_instance()
 
-#with keyboard.GlobalHotKeys({'m': execute}) as h:
-    #h.join()
+
+
+def execute_rsg():
+    if minecraft_window_focused():
+        press_key(Key.tab)
+        press_key(Key.enter)
+        for i in range(3):
+            press_key(Key.tab)
+        press_key(Key.enter)
+        for i in range(2):
+            press_key(Key.tab)
+        for i in range(3):
+            press_key(Key.enter)
+        for i in range(5):
+            press_key(Key.tab)
+        press_key(Key.enter)
+
+        if multi_instance_enabled:
+            multi_instance.switch_to_next_instance()
+
+
+def execute_duo_rsg():
+    if minecraft_window_focused():
+        press_key(Key.tab)
+
+        press_key(Key.enter)
+        for i in range(3):
+            press_key(Key.tab)
+        press_key(Key.enter)
+        for i in range(2):
+            press_key(Key.tab)
+        for i in range(3):
+            press_key(Key.enter)
+        press_key(Key.tab)
+        press_key(Key.enter)
+        for i in range(4):
+            press_key(Key.tab)
+        press_key(Key.enter)
+
+        if multi_instance_enabled:
+            multi_instance.switch_to_next_instance()
+
+def execute_publish():
+    press_key('t')
+    time.sleep(0.1)
+    type_string('/publish 25565')
+    press_key(Key.enter)
+
+
+if multi_instance_enabled:
+    multi_instance.auto_detect_instances()
+
+with keyboard.GlobalHotKeys({'t': execute_rsg
+                             ,
+                            ']': execute_publish}) as h:
+    h.join()
