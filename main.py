a02f90486d647bcfe8ab8b242c9335709893418d
Juvoo <lkinghouser@gmail.com>
2022-12-30 14:17:13 -0600

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, pynput.mouse, subprocess, psutil
+import time, pynput.mouse, subprocess, psutil, logging
 
 from pynput import keyboard
 from pynput.keyboard import Key
@@ -16,6 +16,7 @@ seed = taigaSeed
 
 multi_instance_enabled = True
 speak_info = False
+freeze_instances = False
 
 instances = []
 
@@ -49,9 +50,8 @@ def type_string(string):
 def minecraft_window_focused():
     try:
         focused_window_pid = subprocess.check_output(['osascript', '-e', 'tell application "System Events"', '-e', 'set pid to unix id of first application process whose frontmost is true', '-e', 'return pid', '-e', 'end tell'])
-        process = psutil.Process(int(filter(str.isdigit, focused_window_pid.__str__()).__str__()))
-        p = subprocess.check_output(['osascript', '-e', 'tell application "System Events"', '-e', 'set windowTitle to name of (front window of (first application process whose frontmost is true))', '-e', 'return windowTitle', '-e', 'end tell'])
-        return "Minecraft" in p.__str__()
+        process = psutil.Process(int(focused_window_pid))
+        return process.pid.__str__() in instances
     except subprocess.CalledProcessError:
         return False
 
@@ -140,50 +140,65 @@ def switch_to_next_instance():
 
 
 def auto_detect_instances():
-    instances.clear()
-    print("Detecting Minecraft instances...")
-    if speak_info:
-        subprocess.run(['say', 'Detecting Minecraft instances'])
-    for process in psutil.process_iter():
-        try:
-            if "minecraft" in process.open_files().__str__():
-                window_name = subprocess.check_output(['osascript', '-e', 'tell application "System Events"', '-e', 'set windowTitle to name of (front window of (first application process whose unix id is ' + process.pid.__str__() + '))', '-e', 'return windowTitle', '-e', 'end tell']).__str__()
-                if "minecraft" or "Minecraft" in window_name:
-                    instances.append(process.pid.__str__())
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+    if multi_instance_enabled:
+        instances.clear()
+        print("Detecting Minecraft instances...")
+        if speak_info:
+            subprocess.run(['say', 'Detecting Minecraft instances'])
+        for process in psutil.process_iter():
+            try:
+                if "minecraft" in process.open_files().__str__():
+                    try:
+                        window_name = subprocess.check_output(['osascript', '-e', 'tell application "System Events"', '-e', 'set windowTitle to name of (front window of (first application process whose unix id is ' + process.pid.__str__() + '))', '-e', 'return windowTitle', '-e', 'end tell']).__str__()
+                        if "minecraft" or "Minecraft" in window_name:
+                            instances.append(process.pid.__str__())
+                    except subprocess.CalledProcessError:
+                        logging.exception('\033[1;31m' + 'Encountered a problem while detecting instances. Make sure all Minecraft instances are not in fullscreen mode and are unfrozen.' + '\x1b[0m')
+                        pass
+            except (subprocess.CalledProcessError, psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
 
-    num_instances = instances.__len__().__str__()
-    print("Found " + num_instances + " instance(s)")
-    for instance in instances:
-        print(instance)
+        num_instances = instances.__len__().__str__()
+        print("Found " + num_instances + " instance(s)")
+        for instance in instances:
+            print(instance)
 
-    if speak_info:
-        subprocess.run(['say', 'Found ' + num_instances + ' instances'])
+        if speak_info:
+            subprocess.run(['say', 'Found ' + num_instances + ' instances'])
 
 
 def get_focused_instance_pid():
     if minecraft_window_focused():
         try:
             pid = subprocess.check_output(['osascript', '-e', 'tell application "System Events"', '-e', 'set pid to unix id of first application process whose frontmost is true', '-e', 'return pid', '-e', 'end tell'])
-            return pid
+            process = psutil.Process(int(pid))
+            return process.pid
         except subprocess.CalledProcessError:
             return None
     else:
         return None
 
 
-def toggle_freeze_inactive_instances():
-    current_instance_pid = get_focused_instance_pid()
-    print(current_instance_pid)
-    for pid in instances:
-        if pid not in filter(str.isdigit, current_instance_pid.__str__()):
-            process = psutil.Process(int(pid))
-            status = process.status()
-            if 'stopped' in status:
-                process.resume()
-            else:
+def freeze_inactive_instances():
+    if multi_instance_enabled and freeze_instances:
+        current_instance_pid = get_focused_instance_pid()
+        for pid in instances:
+            current_instance_pid = ''.join(filter(str.isdigit, current_instance_pid.__str__()))
+            print(current_instance_pid)
+            if pid != current_instance_pid:
+                process = psutil.Process(int(pid))
                 process.suspend()
+
+
+def unfreeze_inactive_instances():
+    if multi_instance_enabled and freeze_instances:
+        current_instance_pid = get_focused_instance_pid()
+        for pid in instances:
+            current_instance_pid = ''.join(filter(str.isdigit, current_instance_pid.__str__()))
+            print(current_instance_pid)
+            if pid != current_instance_pid:
+                process = psutil.Process(int(pid))
+                process.resume()
 
 
 if __name__ == '__main__':
@@ -191,5 +206,5 @@ if __name__ == '__main__':
         auto_detect_instances()
 
 
-with keyboard.GlobalHotKeys({'[': execute_rsg, ']': execute_publish, '.': auto_detect_instances, '=': toggle_freeze_inactive_instances}) as h:
-    h.join()
+with keyboard.GlobalHotKeys({'[': execute_rsg, ']': execute_publish, '.': auto_detect_instances, '-': freeze_inactive_instances, '=': unfreeze_inactive_instances}) as listener:
+    listener.join()
